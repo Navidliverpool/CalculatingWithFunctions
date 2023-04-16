af72f6694a9a9f84bed47215640c42870ace8a3a
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-16 22:44:00 +0100

    solution10 added.

---
+++ solution10.js
@@ -0,0 +1,72 @@
+// From Codewars.
+function zero(n) {
+    if (n == null) {
+        return 0
+    } else return parseInt(eval(0 + n))
+}
+
+function one(n) {
+    if (n == null) {
+        return 1
+    } else return parseInt(eval(1 + n))
+}
+
+function two(n) {
+    if (n == null) {
+        return 2
+    } else return parseInt(eval(2 + n))
+}
+
+function three(n) {
+    if (n == null) {
+        return 3
+    } else return parseInt(eval(3 + n))
+}
+
+function four(n) {
+    if (n == null) {
+        return 4
+    } else return parseInt(eval(4 + n))
+}
+
+function five(n) {
+    if (n == null) {
+        return 5
+    } else return parseInt(eval(5 + n))
+}
+
+function six(n) {
+    if (n == null) {
+        return 6
+    } else return parseInt(eval(6 + n))
+}
+
+function seven(n) {
+    if (n == null) {
+        return 7
+    } else return parseInt(eval(7 + n))
+}
+
+function eight(n) {
+    if (n == null) {
+        return 8
+    } else return parseInt(eval(8 + n))
+}
+
+function nine(n) {
+    if (n == null) {
+        return 9
+    } else return parseInt(eval(9 + n))
+}
+
+
+function plus(num) { return '+' + num }
+
+function minus(num) { return '-' + num }
+
+function times(num) { return '*' + num }
+
+function dividedBy(num) { return '/' + num }
+
+
+console.log(one(plus(two())));
