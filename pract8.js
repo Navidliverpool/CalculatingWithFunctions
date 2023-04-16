45a5b74043ee801e0b59a278b65e8e3e85aa8b8d
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-16 22:35:19 +0100

    solution2 added.

--- pract8.js
+++ pract8.js
@@ -1,27 +1,44 @@
 function four(i) {
     // I have changed if (i > 0 && i == plus()) to if (i > 0 && plus()) and it solved the issue. 
-    if (i > 0 && plus()) {
+    if (i === plus) {
         return i + 4;
     }
+    // if (i > 0) {
+    //     return i - 4;
+    // }
     return 4;
 }
 
 function nine(i) {
-    if (i > 0 && plus()) {
-        return i + 9;
-    }
-
+    // if (i === plus()) {
+    //     return i + 9;
+    // }
+    // if (i > 0 && minus()) {
+    //     return i - 9;
+    // }
     return 9;
 }
 
 function plus(i) {
-    if (i == nine()) {
-        return nine();
+    if (i === nine) {
+        console.log("inja1")
+        return nine;
     }
-    if (i == four()) {
-        return nine();
+    if (i === four) {
+        console.log("inja2")
+        return four;
     }
     return "zart";
 }
+
+// function minus(i) {
+//     if (i == nine()) {
+//         return nine();
+//     }
+//     if (i == four()) {
+//         return nine();
+//     }
+//     return "zart";
+// }
 
 console.log(four(plus(nine())))
