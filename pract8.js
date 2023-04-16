58028a8e5ff01040c8b8e60b205ebd992b4db8db
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-16 17:10:58 +0100

    pract8 is updated.

--- pract8.js
+++ pract8.js
@@ -1,19 +1,28 @@
 function four(i) {
-    console.log("inja1", typeof(i), i)
-        // I have changed return plus() + 4; to return i + 4; and it solved the issue. 
+    // I have changed return plus() + 4; to return i + 4; and it solved the issue. 
+
+    if (i > 0) {
+        return i + 4;
+    }
     return i + 4;
 }
 
 function nine(i) {
-    if (i == four()) {
+    // console.log("inja2", i)
+    if (i > 0) {
         return i + 9;
     }
+
     return 9;
 }
 
 function plus(i) {
     if (i == nine()) {
-        console.log("inja3", i)
+        // console.log("inja3", i)
+        return nine();
+    }
+    if (i == four()) {
+        // console.log("inja3", i)
         return nine();
     }
     return "zart";
