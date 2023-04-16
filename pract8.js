6c08c162feb42069eadb84ce82ae7d717974d428
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-16 18:11:26 +0100

    pract8 has been updated.     // I have changed if (i > 0 && i == plus()) to if (i > 0 && plus()) and it solved the issue.

--- pract8.js
+++ pract8.js
@@ -1,15 +1,13 @@
 function four(i) {
-    // I have changed return plus() + 4; to return i + 4; and it solved the issue. 
-
-    if (i > 0) {
+    // I have changed if (i > 0 && i == plus()) to if (i > 0 && plus()) and it solved the issue. 
+    if (i > 0 && plus()) {
         return i + 4;
     }
-    return i + 4;
+    return 4;
 }
 
 function nine(i) {
-    // console.log("inja2", i)
-    if (i > 0) {
+    if (i > 0 && plus()) {
         return i + 9;
     }
 
@@ -18,13 +16,12 @@ function nine(i) {
 
 function plus(i) {
     if (i == nine()) {
-        // console.log("inja3", i)
         return nine();
     }
     if (i == four()) {
-        // console.log("inja3", i)
         return nine();
     }
     return "zart";
 }
+
 console.log(four(plus(nine())))
